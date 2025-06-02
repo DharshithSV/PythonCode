af2488118fbfd98ebd7373d50891aeb715e3cffc
DharshithSV <dharshith.shiva@gmail.com>
2025-06-02 10:33:18 +0530

    Create List Exercise.py

---
+++ List Exercise.py
@@ -0,0 +1,13 @@
+listx =[12,13,14,16,19,18,23,26]
+
+even = []
+odd  = []
+
+for item in listx:
+    if item % 2 ==0:
+        even.append(item)
+    else:
+        odd.append(item)
+
+print('Even list = ',even)
+print('Odd list = ',odd)
